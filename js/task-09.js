f560a832052e5a1f4a631a6f5346d47af97fd237
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-13 20:33:26 -0900

    Completion of the ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,29 @@
+// Write a script that changes the background colors of the <body> element via inline style when clicking on button.change-color and outputs the color value to span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Use the getRandomHexColor function to generate a random color.
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector("body"),
+  colorDisplay = document.querySelector(".color"),
+  changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorDisplay.textContent = randomColor;
+});
